156316a61f614e2f685c17f1001fed30ba1e48ee
cs876321722@outlook.com <cs876321722@outlook.com>
2017-10-20 17:05:49 +0800

    log

--- app/src/main/java/com/cs/controller/GlobalExceptionController.java
+++ app/src/main/java/com/cs/controller/GlobalExceptionController.java
@@ -1,5 +1,7 @@
 package com.cs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +11,8 @@ import com.cs.dto.Message;
 @ControllerAdvice
 public class GlobalExceptionController {
 
+	private Logger logger = LoggerFactory.getLogger(GlobalExceptionController.class);
+	
 	@ResponseBody
     @ExceptionHandler(Exception.class)
     public Message handleException(Exception e) {
@@ -17,7 +21,8 @@ public class GlobalExceptionController {
         if (msg == null || msg.equals("")) {
         	return new Message(0, "", "服务器出错");
         }
-		return new Message(0, "", msg);
+        logger.error(msg);
+		return new Message(0, "", "叫后台小哥查日志");
         
     }
 
